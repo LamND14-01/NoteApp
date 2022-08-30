7007a7c36b960d290c7a57edcd768655c0153874
LamND <lamnd.eloo.dev@gmail.com>
2022-08-30 20:26:16 +0700

    Initial Commit

---
+++ Shared/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  Shared
+//
+//  Created by Eloo on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
